49b584bc5c35c94b8c87f6bd601683aabf53a654
John Loucks <johnloucks@gmail.com>
2020-08-19 13:26:58 -0500

    minor update adding row append

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,25 +3,26 @@ import csv
 
 finance_csv = os.path.join('..', 'Resources','budget_data.csv')
 
+# Read in the CSV file
+with open(finance_csv, 'r') as csvfile:
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+    date = []
+    for row in csvreader:
+        date.append(row[0])
+
+
 def print_analysis(finance_data):
     date = str(finance_data[0])
     balance = int(finance_data[1])
 
-    total_months = len(date)
-    total = sum(balance)
+    #total_months = len(date)
+    #total = sum(balance)
     #daily_change = (current day - previous day(skipping day 1))
-    #average_change = avg(daily_change?)
+    #average_change = avg(daily_change?)     
 
 
-     
-
-# Read in the CSV file
-with open(finance_csv, 'r') as csvfile:
-
-    # Split the data on commas
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    header = next(csvreader)
 
 # Print out the anaylsis
     print ("Financial Analysis")
