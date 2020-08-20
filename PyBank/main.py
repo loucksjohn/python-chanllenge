c2faea8c2328dfa5b71ef9d6c7c4ede1705063d0
John Loucks <johnloucks@gmail.com>
2020-08-20 16:09:50 -0500

    counts total months and sums total dollars

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,35 +3,30 @@ import csv
 
 finance_csv = os.path.join('..', 'Resources','budget_data.csv')
 
+
 # Read in the CSV file
 with open(finance_csv, 'r') as csvfile:
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
-    date = []
-    for row in csvreader:
-        date.append(row[0])
-
+    date = [] 
     balance = []
     for row in csvreader:
-        balance.append(row[1])
-
-    total_months = sum(date)
+        date.append(row[0])
+        balance.append(int(row[1]))   
+    
+    
+    total_months = len(date)
     total_dollars = sum(balance)
 
-
-
-    #total_months = len(date)
-    #total = sum(balance)
     #daily_change = (current day - previous day(skipping day 1))
-    #average_change = avg(daily_change?)     
-
+    #average_change = avg(daily_change?)  
 
 
 # Print out the anaylsis
     print ("Financial Analysis")
     print ("-----------------------------") 
-    print(f"Total Months: (str{total_months})")
+    print(f"Total Months: {str(total_months)}")
     print(f"Total: {str(total_dollars)}")
     #(f"Average Change: {str(average_change)}")
     #print(f"Greatest Increase in Profits: {str("date and (amount)")}")
